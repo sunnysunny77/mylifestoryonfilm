e1bbc190ea182dee6811ba0f356da77056e18d49
daniel <contractor@lateralaspect.com.au>
2025-03-24 16:59:36 +0800

    header

--- site/index.php
+++ site/index.php
@@ -17,9 +17,131 @@
 
     <main>
 
-      <div class="container">
+      <div class="container-fluid header-brochure">
 
-        <div class="row justify-content-center">
+        <div class="row justify-content-between">
+
+          <div class="d-done d-sm-block col-sm-14 header-brochure-side-column"></div>
+
+          <div class="col-48 col-md-30 px-8 pt-12">
+
+            <div class="row">
+
+              <div class="col-42">
+
+                <h1 class="text-end">
+
+                  <span>
+
+                    My Life Story
+
+                  </span>
+
+                  <span>
+
+                    On Film
+
+                  </span>
+
+                </h1>
+
+              </div>
+
+              <div class="col-42 pb-10">
+
+                <p class="text-end">
+
+                  SHARE YOUR MEMORIES
+
+                </p>
+
+              </div>
+
+              <div class="col-48 pb-10">
+
+                <div class="row justify-content-between">
+
+                  <div class="col-14 primary-border p-3 p-sm-4">
+
+                    <img src="./images/outside.webp" alt="Mother and son walking" width="300" height="300">
+
+                  </div>
+
+                  <div class="col-14 primary-border p-3 p-sm-4">
+
+                    <img src="./images/groovy.webp" alt="People having a groovy time" width="300" height="300">
+
+                  </div>
+
+                  <div class="col-14 primary-border p-3 p-sm-4">
+
+                    <img src="./images/yoga.webp" alt="People doing yoga" width="300" height="300">
+
+                  </div>
+
+                </div>
+
+              </div>
+
+            </div>
+
+          </div>
+
+          <div class="col-48 header-brochour-aside py-9 px-md-8">
+
+            <h2 class="mb-5 mt-7 ms-7 ms-md-10">
+
+              Get in touch
+
+            </h2>
+
+            <div class="row justify-content-center justify-content-md-evenly">
+
+              <div class="col-39 col-sm-16 col-xl-12 py-5 px-sm-7 px-md-10">
+
+                <i class="fa-solid fa-location-dot"></i>
+
+                <hr class="my-3">
+
+                <address>
+
+                  <span> 1 More Av Warwich </span> <span> 6088 </span> <span> WA PERTH </span>
+
+                </address>
+
+              </div>
+
+              <div class="col-39 col-sm-16 col-xl-12 py-5 px-sm-7 px-md-10">
+
+                <i class="fa-solid fa-phone"></i>
+
+                <hr class="my-3">
+
+                <a href="tel:0456965365">
+
+                  0456965365
+
+                </a>
+
+              </div>
+
+              <div class="col-39 col-sm-16 col-xl-12 py-5 px-sm-7 px-md-10">
+
+                <i class="fa-solid fa-envelope"></i>
+
+                <hr class="my-3">
+
+                <a href="mail:eg@example.com">
+
+                  eg@example.com
+
+                </a>
+
+              </div>
+
+            </div>
+
+          </div>
 
         <div>
 
